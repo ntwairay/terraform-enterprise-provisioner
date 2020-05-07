4c2907181f9c25d87cd23f130aeb46b395146592
Ray Ng <ray.ng@gradeous.com.au>
2020-05-08 03:56:41 +1000

    module version 3

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
 resource "tfe_workspace" "example" {
   name         = var.workspace_name
   organization = var.organization
+  vcs_repo  {
+    identifier = var.vcs_repo.identifier
+    branch     = var.vcs_repo.branch
+    oauth_token_id = var.vcs_repo.oauth_token_id
+  }
+  working_directory = var.working_directory
+}
+
+resource "tfe_variable" "example" {
+  key          = var.secret_key
+  value        = var.secret_value
+  workspace_id = tfe_workspace.example.id
+  sensitive    = var.sensitive
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,31 @@
 variable "workspace_name" {
-  default = ""
+  default = "vibrato"
 }
 
 variable "organization" {
   default = ""
+}
+
+variable "vcs_repo" {
+  type = object({
+    identifier  = string
+    branch      = string
+    oauth_token_id = string
+  })
+}
+
+variable "working_directory" {
+  default = ""
+}
+
+variable "secret_key" {
+  default = ""
+}
+
+variable "secret_value" {
+  default = ""
+}
+
+variable "sensitive" {
+  default = true
 }
